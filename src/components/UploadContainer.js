ced8fa3928e0b3847ac729972db0c82c63e47571
Faizaan shaikh <sfaizan974@gmail.com>
2022-02-20 23:14:23 +0530

    Adding Small Progress bar

--- src/components/UploadContainer.js
+++ src/components/UploadContainer.js
@@ -8,6 +8,7 @@ const UploadContainer = () => {
   const host = "https://innshare.herokuapp.com";
   const uploadURL = `${host}/api/files`;
   const [classNames, setclassNames] = useState("drop-zone");
+  const [percent, setpercent] = useState(0);
   const fileInput = document.getElementsByTagName("input")[0];
 
   const handleOnClick = () => {
@@ -58,9 +59,16 @@ const UploadContainer = () => {
     const file = fileInput.files[0];
     const formData = new FormData();
     formData.append("myfile", file);
+
     const updateProgress = (e) => {
-      console.log(e);
+      const bgProgress = document.querySelector(".bg-progress");
+      // console.log(e.loaded);
+      const percentCompleted = Math.floor((e.loaded * 100) / e.total);
+      console.log(percentCompleted);
+      bgProgress.style.width = `${percentCompleted}%`;
+      setpercent(percentCompleted);
     };
+
     const config = {
       onUploadProgress: updateProgress,
     };
@@ -105,6 +113,16 @@ const UploadContainer = () => {
             <span className="browsebtn" onClick={handleOnClick}>
               browse
             </span>
+          </div>
+        </div>
+        <div className="progress-container">
+          <div className="bg-progress"></div>
+          <div className="inner-container">
+            <div className="title">Uploading...</div>
+            <div className="percent-container">
+              <span id="percent">{percent}</span>%
+            </div>
+            <div className="progress-bar"></div>
           </div>
         </div>
       </section>
